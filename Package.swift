b4afa814d3eeb65822ab1ecc5f18050b30eb6a41
Jessy Catterwaul <jessy@catterwaul.com>
2024-06-11 06:30:33 -0400

    Create package

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version: 6.0
+
+import PackageDescription
+
+let name = "Thrappture"
+
+_ = Package(
+  name: name,
+  products: [.library(name: name, targets: [name])],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-docc-plugin", branch: "main")
+  ],
+  targets: [
+    .target(name: name),
+    .testTarget(
+      name: name + ".Tests",
+      dependencies: [.init(stringLiteral: name)]
+    )
+  ]
+)
