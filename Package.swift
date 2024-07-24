680444571be300d173d6a1997e8818f18a6ee715
Jessy Catterwaul <jessy@catterwaul.com>
2024-07-23 20:28:16 -0400

    Set all minimum supported platforms
    Necessary for opaque types and concurrency.

--- Package.swift
+++ Package.swift
@@ -1,14 +1,16 @@
 // swift-tools-version: 6.0
 
+import Foundation // for String.capitalized
 import PackageDescription
 
 let name = "Thrappture"
 
 _ = Package(
   name: name,
+  platforms: [.iOS(.v13), .macOS(.v10_15)],
   products: [.library(name: name, targets: [name])],
   dependencies: [
-    .package(url: "https://github.com/apple/swift-docc-plugin", branch: "main")
+    .package(url: "https://github.com/swiftlang/swift-docc-plugin", branch: "main")
   ],
   targets: [
     .target(name: name),
@@ -18,3 +20,48 @@ _ = Package(
     )
   ]
 )
+
+// MARK: - Dependency
+
+nonisolated var dependencies: [Dependency]  {
+  [ .swift(repositoryName: "docc-plugin")
+  ]
+}
+
+struct Dependency {
+  let package: Package.Dependency
+  let product: Target.Dependency
+}
+
+extension Dependency {
+  static func apple(repositoryName: String) -> Self {
+    .swift(organization: "apple", repositoryName: repositoryName)
+  }
+
+  static func swift(organization: String = "swiftlang", repositoryName: String) -> Self {
+    .init(
+      organization: organization,
+      name: repositoryName.split(separator: "-").map(\.capitalized).joined(),
+      repositoryName: "swift-\(repositoryName)"
+    )
+  }
+
+  static func catterwaul(name: String, repositoryName: String? = nil, branch: String? = nil) -> Self {
+    .init(
+      organization: "Catterwaul",
+      name: name,
+      repositoryName: repositoryName ?? name,
+      branch: branch
+    )
+  }
+
+  private init(organization: String, name: String, repositoryName: String, branch: String? = nil) {
+    self.init(
+      package: .package(
+        url: "https://github.com/\(organization)/\(repositoryName)",
+        branch: branch ?? "main"
+      ),
+      product: .product(name: name, package: repositoryName)
+    )
+  }
+}
