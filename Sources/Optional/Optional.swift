15fbb37bc5c197bd0e97c814becbe677bd9f6561
Jessy Catterwaul <jessy@catterwaul.com>
2025-03-13 04:34:20 -0400

    Change the last remaining mention of `UnwrapError` to `Nil`

--- Sources/Optional/Optional.swift
+++ Sources/Optional/Optional.swift
@@ -5,7 +5,7 @@ extension Optional: ThrowingPropertyWrapper {
   }
   
   /// The `get` accessor for the wrapped value.
-  /// - Bug: [We can't use `throws(UnwrapError)`](https://github.com/apple/swift/issues/74289).
+  /// - Bug: [We can't use `throws(Nil)`](https://github.com/apple/swift/issues/74289).
   /// Until that is fixed, `Optional.Error` remains `any Error`, not `Nil`, as it should be.
   /// - Throws: `Nil`
   @inlinable public func get() throws -> Wrapped {
