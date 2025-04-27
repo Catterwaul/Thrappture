ed98ac58ca5d4dddbe27119adc644b0a05b71aad
Jessy Catterwaul <jessy@catterwaul.com>
2025-04-27 10:50:05 -0400

    Move code to HMOptional

--- Sources/Optional/Optional.swift
+++ Sources/Optional/Optional.swift
@@ -19,20 +19,3 @@ extension Optional: ThrowingPropertyWrapper {
     self = try? newValue()
   }
 }
-
-// MARK: - public
-public extension Optional {
-  /// Exchange a tuple of `Optional`s for a single `Optional` whose `Wrapped` is a tuple.
-  /// - Returns: `nil` if any tuple element is `nil`.
-  @inlinable static func zip<each _Wrapped>(_ optional: (repeat (each _Wrapped)?)) -> Self
-  where Wrapped == (repeat each _Wrapped) {
-    try? (repeat (each optional).get())
-  }
-
-  /// Transform `.some` into `.none`, if a condition fails.
-  /// - Parameters:
-  ///   - isSome: The condition that will result in `nil`, when evaluated to `false`.
-  func filter<Error>(_ isSome: (Wrapped) throws(Error) -> Bool) throws(Error) -> Self {
-    try flatMap { wrapped throws(Error) in try isSome(wrapped) ? wrapped : nil }
-  }
-}
